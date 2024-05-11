9a0ebb77c303f83729fe946b61fb69a768648227
Thomas Graham <thomas.graham18@outlook.com>
2024-05-11 01:29:50 -0300

    clean and format

--- src/commands/music/shuffle.js
+++ src/commands/music/shuffle.js
@@ -1,47 +1,45 @@
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
 /**
  * @description Shuffle the queue
  */
 module.exports = {
-	// Slash command properties
-	data: new SlashCommandBuilder()
-		.setName('shuffle')
-		.setDescription('Shuffle the queue'),
+  // Slash command properties
+  data: new SlashCommandBuilder()
+    .setName("shuffle")
+    .setDescription("Shuffle the queue"),
 
-	// Command execution
-	async execute(interaction, client) {
-		await interaction.deferReply({ ephemeral: false });
+  // Command execution
+  async execute(interaction, client) {
+    await interaction.deferReply({ ephemeral: false });
 
-		const queue = client.distube.getQueue(interaction);
+    const queue = client.distube.getQueue(interaction);
 
-		// Check if queue is empty
-		if (!queue)
-			return interaction.editReply(
-				`There is nothing in the queue right now!`
-			);
+    // Check if queue is empty
+    if (!queue)
+      return interaction.editReply(`There is nothing in the queue right now!`);
 
-		const { channel } = interaction.member.voice;
+    const { channel } = interaction.member.voice;
 
-		// Check if user is in a voice channel
-		if (!channel)
-			return interaction.editReply('You are not in a voice channel!');
+    // Check if user is in a voice channel
+    if (!channel)
+      return interaction.editReply("You are not in a voice channel!");
 
-		// Make sure the bot is in the same channel as the user
-		if (
-			interaction.member.voice.channel !==
-			interaction.guild.members.me.voice.channel
-		)
-			return interaction.editReply(
-				'You are not in the same voice channel as me!'
-			);
+    // Make sure the bot is in the same channel as the user
+    if (
+      interaction.member.voice.channel !==
+      interaction.guild.members.me.voice.channel
+    )
+      return interaction.editReply(
+        "You are not in the same voice channel as me!"
+      );
 
-		await client.distube.shuffle(interaction);
+    await client.distube.shuffle(interaction);
 
-		const embed = new EmbedBuilder()
-			.setColor(client.colour)
-			.setDescription(`🔀 | **Songs have been shuffled**`);
+    const embed = new EmbedBuilder()
+      .setColor(client.colour)
+      .setDescription(`🔀 | **Songs have been shuffled**`);
 
-		interaction.editReply({ embeds: [embed] });
-	},
+    interaction.editReply({ embeds: [embed] });
+  },
 };
