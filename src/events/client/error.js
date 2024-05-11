c3e2e7b85f186b04b05a7a882103c5bdc1adb61f
Thomas Graham <thomas.graham18@outlook.com>
2024-05-11 01:30:45 -0300

    remove embed

--- src/events/client/error.js
+++ src/events/client/error.js
@@ -1,22 +1,16 @@
-const { blue, red } = require('chalk');
+const { EmbedBuilder } = require("discord.js");
+const { red, blue } = require("chalk");
 
 module.exports = async (client, channel, error) => {
-	console.log(
-		red(`[ERROR] `) +
-			'An error event was sent by Discord.js: \n' +
-			blue('[INFO]: ') +
-			client.user.tag +
-			' ' +
-			client.user.id +
-			' ' +
-			blue('[ERROR]: ') +
-			error
-	);
-
-	const embed = new EmbedBuilder()
-		.setTitle('** ⁉️ Error occured **')
-		.setColor(0xff0000)
-		.setDescription(`${error}`);
-
-	channel.send({ embeds: [embed] });
+  console.log(
+    red(`[ERROR] `) +
+      "An error event was sent by Discord.js: \n" +
+      blue("[INFO]: ") +
+      client.user.tag +
+      " " +
+      client.user.id +
+      " " +
+      blue("[ERROR]: ") +
+      error
+  );
 };
